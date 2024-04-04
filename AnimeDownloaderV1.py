a0d2f0e2890b2097e1fafbede1372081db025516
dustyash69 <161035719+dustyash69@users.noreply.github.com>
2024-04-04 14:22:36 +0530

    Update AnimeDownloaderV1.py
    Edited script for convenience to user

--- AnimeDownloaderV1.py
+++ AnimeDownloaderV1.py
@@ -9,12 +9,16 @@ def click_and_download_links(url):
 
     try:
         driver.get(url)
-        time.sleep(10)
         links = driver.find_elements(By.XPATH, "//a")
         links = [link for link in links if link != links[0]]
         for link in links:
             link.click()
-            time.sleep(waitTime)
+            while True:
+                files_exist = any(file.endswith(".crdownload") for file in os.listdir(r"C:\Users\win\Downloads"))
+                if files_exist:
+                    time.sleep(5)
+                else:
+                    break
 
     except Exception as e:
         print(f"Error occurred: {e}")
@@ -30,6 +34,4 @@ def click_and_download_links(url):
 if __name__ == "__main__":
     print("Type in an URL like: https://dl.animesp.xyz/Completed/Blend-S/720/ (A click on a link in the webpage should download an episode)")
     start_url = input()
-    print("Time to wait until one episode is downloaded: (Download one episode by yourself on laptop by using edge to get an approximate time)")
-    waitTime = int(input())
     click_and_download_links(start_url)
